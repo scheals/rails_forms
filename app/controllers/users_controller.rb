014c8b4d5a593403388caaec4dcf150b46d31d14
Scheals <95084627+scheals@users.noreply.github.com>
2022-10-22 15:31:23 +0200

    feat: editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def create
     @user = User.new(user_params)
-    # @user = User.new(name: params[:name], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
